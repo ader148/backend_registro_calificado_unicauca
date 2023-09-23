0c9bf2fae48bdf240bda43ff40dfbf1eeae6d500
Alvaro Daniel Erazo Ramirez <alvaro.erazo@siigo.com>
2023-09-22 19:26:27 -0500

    creacion de estructura de proyecto

---
+++ src/main/java/com/unicauca/backend_registro_calificado/BackendRegistroCalificadoApplication.java
@@ -0,0 +1,13 @@
+package com.unicauca.backend_registro_calificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRegistroCalificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRegistroCalificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicauca/backend_registro_calificado/BackendRegistroCalificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicauca.backend_registro_calificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRegistroCalificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
